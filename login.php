4da18132c418b5f625a09e5cf8702552bcfef75c
Pierce75 <23376783@studentmail.ul.ie>
2026-03-23 19:03:48 +0000

    test autoscript deploy

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if (!empty($_SESSION['user_id'])) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>HarmonyMatch — Login / Register</title>
+  <title>HarmonyMatchhhh — Login / Register</title>
   <link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/styles.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
   <style>
